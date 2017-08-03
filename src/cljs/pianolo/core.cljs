b8844465c9a183ebe2e9949251b1ee3644a76773
Saskia Lindner <saskia.li@fu-berlin.de>
2017-08-03 15:34:39 +0200

    Enable adding, removing and editing of skill level

--- src/cljs/pianolo/core.cljs
+++ src/cljs/pianolo/core.cljs
@@ -1,36 +1,68 @@
 (ns pianolo.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [pianolo.components.svg :as svg]))
+
 
 (enable-console-print!)
 
 (defonce app-state (r/atom {:input ""
-                            :pieces [{:title "Toccata"}]}))
+                            :pieces [{:id (random-uuid)
+                                      :title "Toccata"
+                                      :level 9}]}))
 
-(defn title []
-  [:div.title
+(defn header-title []
+  [:div.header-title
    [:h1 "welcome to pianolo"]])
 
-(defn input-field []
-  (let [pieces-cursor (r/cursor app-state [:pieces ])]
-    [:div.input-field
-     [:input {:type "field"
-              :value (:input @app-state)
-              :on-change #(swap! app-state assoc :input (-> % .-target .-value))
-              :on-key-down #(if (= 13 (.-which %))
-                              (do
-                                (swap! app-state update-in [:pieces] conj {:title (-> % .-target .-value)})
-                                (swap! app-state assoc :input "")))}]]))
+(defn input-field [rep-state]
+  [:div.input-field
+   [:input {:type "field"
+            :value (:input @rep-state)
+            :on-change #(swap! rep-state assoc :input (-> % .-target .-value))
+            :on-key-down #(if (= 13 (.-which %))
+                            (do
+                              (swap! rep-state assoc :input "")
+                              (swap! rep-state update :pieces conj {:id (random-uuid)
+                                                                    :title (-> % .-target .-value)
+                                                                    :level 1})))}]])
 
-(defn repertoire [app-state]
+(defn skill [rep-state idx]
+  (let [level-cur (r/cursor rep-state [:pieces idx :level])]
+    [:div.skill
+     (if (> @level-cur 1)
+       [:div.icon-circle
+        {:type "button"
+         :on-click (fn [] (swap! level-cur dec))}
+        [svg/CircleLeftIcon]])
+     [:div.level @level-cur]
+     (if (< @level-cur 10)
+       [:div.icon-circle
+        {:type "button"
+         :on-click (fn [] (swap! level-cur inc))}
+        [svg/CircleRightIcon]])]))
+
+(defn repertoire-item [rep-state idx]
+  (let [title (get-in @rep-state [:pieces idx :title])]
+    [:div.piece
+     [:div.name title]
+     [skill rep-state idx]
+     [:div.icon-remove {:type "button"
+                        :on-click (fn []
+                                    (swap! rep-state update :pieces
+                                           #(vec (remove (fn [item] (= title (:title item))) (:pieces @rep-state)))))}
+                       [svg/RemoveIcon]]]))
+
+(defn repertoire [rep-state]
   [:div.repertoire
-   (for [piece (:pieces @app-state)]
-     [:div.piece (:title piece)])])
+   (map-indexed (fn [idx {id :id}]
+                  ^{:key id} [repertoire-item rep-state idx])
+                (:pieces @rep-state))])
 
-(defn panel [app-state]
+(defn panel [rep-state]
   [:div.panel
-   [title]
-   [input-field]
-   [repertoire app-state]])
+   [header-title]
+   [input-field rep-state]
+   [repertoire rep-state]])
 
 (defn render []
-  (r/render [panel app-state] (js/document.getElementById "app")))
+  (r/render [panel app-state] (.getElementById js/document "app")))
